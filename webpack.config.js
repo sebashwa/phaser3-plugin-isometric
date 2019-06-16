f4d94dc865a3155e0674a5c9458aa2e2027bdb2c
Daniel W. Chen <hello@danielwjchen.com>
2019-06-15 21:16:21 -0500

    Upgrades babel and webpack to the latest version.

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,16 @@ module.exports = {
   ],
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+      {
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env'
+          ]
+        }
+      }
     ]
   },
   output: {
